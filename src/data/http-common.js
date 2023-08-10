174e2f92faa63848f1ce878c79ce1ab725a32a3b
anapatino <asofiapatino@unicesar.edu.co>
2023-08-10 11:58:57 -0500

    url

--- src/data/http-common.js
+++ src/data/http-common.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://localhost:7239/",
+  baseURL: "https://fafz3q9ih5.execute-api.us-east-2.amazonaws.com/pegi/",
 });
-
-/*
-export default axios.create({
-  baseURL: "https://peluquitamacarron.tk/",
-});
-
-*/
