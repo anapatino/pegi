16b4d134d8e20cff21191161196ec051b3ce92f1
anapatino <asofiapatino@unicesar.edu.co>
2023-08-11 22:01:38 -0500

    url again

--- src/data/http-common.js
+++ src/data/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fafz3q9ih5.execute-api.us-east-2.amazonaws.com/pegi/",
+  baseURL: "https://fafz3q9ih5.execute-api.us-east-2.amazonaws.com/pegi",
 });
